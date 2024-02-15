8c46e7c81b3dd14027da1be8928430479c7e4f7d
MasapakaRaju <masapakaraju28@gmail.com>
2024-02-15 10:54:20 +0530

    added variables.js

---
+++ variables.js
@@ -0,0 +1,11 @@
+console.log("let is a global variable in which we can change the type of the variable")
+let x=1
+console.log(x,typeof(x))
+x="xyz"
+console.log(x,typeof(x))
+
+console.log("\nvar has a scope for its values we cannot change the  types")
+var y=1
+console.log(y,typeof(y))
+y="xyz"
+console.log(y,typeof(y))
