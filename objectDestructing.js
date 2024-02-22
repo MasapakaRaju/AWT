94e398eec095f45e70d52e9f1a3a0ed80d0da0eb
MasapakaRaju <masapakaraju28@gmail.com>
2024-02-22 10:41:12 +0530

    objectDestructing.js modified

---
+++ objectDestructing.js
@@ -0,0 +1,42 @@
+const vehicle = {
+    brand: 'Ford',
+    model: 'Mustang',
+    type: 'car',
+    year: 2021, 
+    color: 'red',
+    registration: {
+      city: 'Houston',
+      state: 'Texas',
+      country: 'USA'
+    }
+  }
+  
+  const vehicleOne = {
+    brand: 'Ford',
+    model: 'Mustang',
+    type: 'car',
+    year: 2021, 
+    color: 'red'
+  }
+  
+  myVehicle(vehicle);
+  
+  // old way
+  function myVehicle(vehicle) {
+    const message = 'My ' + vehicle.type + ' is a ' + vehicle.color + ' ' + vehicle.brand + ' ' + vehicle.model + '.';
+    console.log(message);
+  }
+  //new way
+  myVehicle2(vehicle)
+  
+  function myVehicle2({ model, registration: { state } }) {
+    const message = 'My ' + model + ' is registered in ' + state + '.';
+    console.log(message);
+  }
+  
+  myVehicle1(vehicleOne);
+  
+  function myVehicle1({type, color, brand, model}) {
+    const message = 'My ' + type + ' is a ' + color + ' ' + brand + ' ' + model + '.';
+    console.log(message);
+  }
