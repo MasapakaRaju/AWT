10f2fc35fc86eb5fc18c9e94d872686eb979ceee
MasapakaRaju <masapakaraju28@gmail.com>
2024-02-22 10:21:06 +0530

    arrayDestructing.js modified

--- arrayDestructing.js
+++ arrayDestructing.js
@@ -1,12 +1,24 @@
 const array=['banana','carrot','pen']
 //old way to access an array elements
-// const fruit=array[0];
-// const veg=array[1];
-// const obj=array[2];
-// console.log("array elements\n->",fruit,veg,obj)
+const fruit=array[0];
+const veg=array[1];
+const obj=array[2];
+console.log("array elements\n->",fruit,veg,obj)
 
 //********** When destructuring arrays, the order that variables are declared is important. ****************
 
 //new way is destructing
-const [fruit,obj]=array;
-console.log(fruit,obj);
+const [Fruit,,Obj]=array;
+console.log(Fruit,Obj);
+
+function calculate(a, b) {
+    const add = a + b;
+    const subtract = a - b;
+    const multiply = a * b;
+    const divide = a / b;
+  
+    return [add, subtract, multiply, divide];
+  }
+  
+  const [add, subtract, multiply, divide] = calculate(4, 7);
+console.log(add ,subtract,multiply,Math.round(divide));
