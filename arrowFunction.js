f23025308a7babae547fea4f1a8291a60b6b77cf
MasapakaRaju <masapakaraju28@gmail.com>
2024-02-22 09:30:04 +0530

    arrowFunction.js commited

---
+++ arrowFunction.js
@@ -0,0 +1,28 @@
+//Arrows
+//without arrow function
+// hello = function()
+// {
+//     return "Hello World.!"
+// }
+// console.log(hello());
+
+// //with arrow function
+// hello=()=>{
+//     return "Hello World.!"
+// }
+
+// //arrow function return value by default
+// hello = ()=> "Hello World.!";
+// console.log(hello())
+
+// //multiplication using arrow function
+// let myFunction= (a,b)=> a*b;
+// console.log(myFunction(1,4));
+
+// //Arrow function with parameter
+// hello = (val)=> "Hello "+val;
+// console.log(hello("Masapaka"))
+
+//addition with arrow function
+// add = (a,b) => a+b;
+// console.log(add(3,6))
