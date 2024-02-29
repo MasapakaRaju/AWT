9125bc9c5e27badca7aa0695b53727d78b02266d
MasapakaRaju <masapakaraju28@gmail.com>
2024-02-29 10:36:19 +0530

    added FatArrow Function

---
+++ fatArrow.js
@@ -0,0 +1,9 @@
+//fat Arrow function
+//greet = val=> `Hello ${val}`
+console.log(greet('Raju'))
+
+//normal arrow function
+function greet(val){
+    console.log('normal function')
+    return `Hello ${val}`;
+}
